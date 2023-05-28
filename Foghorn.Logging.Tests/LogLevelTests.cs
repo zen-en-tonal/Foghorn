d44e22a9e2bd40ebe96f9601ce079b28871a5a57
Takeru KODAMA <zen.en.tonal@gmail.com>
2023-05-28 21:30:27 +0900

    [Add] test

--- Foghorn.Logging.Tests/LogLevelTests.cs
+++ Foghorn.Logging.Tests/LogLevelTests.cs
@@ -51,4 +51,27 @@ public class LogLevelTests
         // Then
         logOutput.Verify(o => o.Write(It.IsAny<FoghornLog>()), Times.Exactly(2));
     }
+
+    [Fact]
+    public void LogLevel_can_be_duplicated()
+    {
+        // Given
+        var logOutput = new Mock<ILogOutput>();
+        logOutput.Setup(o => o.Write(It.IsAny<FoghornLog>()));
+        var logOutputProvider = new Mock<ILogOutputProvider>();
+        logOutputProvider.Setup(l => l.CreateLogOutput()).Returns(logOutput.Object);
+
+        var logger = new FoghornLoggerBuilder("ident", "host")
+            .AddLogOutput(LogLevel.Information, logOutputProvider.Object)
+            .AddLogOutput(LogLevel.Information, logOutputProvider.Object)
+            .Build();
+
+        // When
+        logger.Debug("debug");
+        logger.Info("info"); // should be called
+        logger.Warning("warning"); // should be called
+
+        // Then
+        logOutput.Verify(o => o.Write(It.IsAny<FoghornLog>()), Times.Exactly(4));
+    }
 }
